53dc7ed2642923d00f6298472d499f552ca38c20
Draxtor <kage.draxtor@gmail.com>
2022-02-07 17:08:49 +0100

    stats display & level up

--- assets/scripts/hero.js
+++ assets/scripts/hero.js
@@ -1,3 +1,60 @@
-var level = 1;
-var exp = 0;
-var hp: 100;
+var level;
+var levelPoints = 0;
+var LEVELPOINTSPERLEVEL = 3;
+var exp;
+var gold;
+var hunterPoint;
+
+//STATS
+var strength;
+var constitution;
+var dexterity;
+var hp;
+
+
+//DYNAMIC APPEND
+var appendAddStr='    <span id ="addStr" class="clickableAwesomeFont"><i class="fa fa-plus-circle"></span>';
+var appendAddCons='    <span id ="addCons" class="clickableAwesomeFont"><i class="fa fa-plus-circle"></span>';
+var appendAddDex='    <span id ="addDex" class="clickableAwesomeFont"><i class="fa fa-plus-circle"></span>';
+
+function heroInit(){
+  level = 1;
+  exp = 0;
+  gold = 0;
+  hunterPoint = 0;
+
+  strength = 1;
+  constitution = 1;
+  dexterity = 1;
+  hp = 100;
+  updateLabelStats();
+}
+
+function levelUp(){
+  level = level + 1;
+  levelPoints = levelPoints + LEVELPOINTSPERLEVEL;
+  $('#lblPointToSPend').removeClass('hide');
+  $('#badgeLevelPointsHero').removeClass('hide');
+  updateLabelStats()
+}
+
+function updateLabelStats(){
+  $('#badgeLevelPointsHero').text(levelPoints);
+  $('#lblPointToSPend').text(levelPoints + ' points to spend')
+  $('#lblLevel').text('Level: ' + level);
+  $('#lblExp').text('Experience: ' + exp);
+  $('#lblGold').text('Gold: ' + gold);
+  $('#lblHunterP').text('Hunter Points: ' + hunterPoint);
+  if(levelPoints > 0){
+    $('#lblStr').text('Strength: ' + strength).append(appendAddStr);
+    $('#lblCons').text('Constitution: ' + constitution).append(appendAddCons);
+    $('#lblDex').text('Dexterity: ' + dexterity).append(appendAddDex);
+  }else{
+    $('#badgeLevelPointsHero').addClass('hide');
+    $('#lblPointToSPend').addClass('hide');
+    $('#lblStr').text('Strength: ' + strength);
+    $('#lblCons').text('Constitution: ' + constitution);
+    $('#lblDex').text('Dexterity: ' + dexterity);
+  }
+  $('#lblHp').text('HP: ' + hp);
+}
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,21 +1,51 @@
 $(function(){
 
+  heroInit();
+
 $('#settingsBtn').click(function(){
 	$('.home').addClass('hide');
   $('.support').addClass('hide');
   $('.settings').removeClass('hide');
+  $('.hero').addClass('hide');
 });
 
 $('#homeBtn').click(function(){
 	$('.settings').addClass('hide');
   $('.support').addClass('hide');
   $('.home').removeClass('hide');
+  $('.hero').addClass('hide');
 });
 
 $('#supportBtn').click(function(){
 	$('.settings').addClass('hide');
   $('.home').addClass('hide');
   $('.support').removeClass('hide');
+  $('.hero').addClass('hide');
+});
+
+$('#heroBtn').click(function(){
+	$('.settings').addClass('hide');
+  $('.home').addClass('hide');
+  $('.support').addClass('hide');
+  $('.hero').removeClass('hide');
+});
+
+$(document).on('click','#addStr',function(){
+  strength=strength+1;
+  levelPoints = levelPoints - 1;
+  updateLabelStats();
+});
+
+$(document).on('click','#addCons',function(){
+  constitution=constitution+1;
+  levelPoints = levelPoints - 1;
+  updateLabelStats();
+});
+
+$(document).on('click','#addDex',function(){
+  dexterity=dexterity+1;
+  levelPoints = levelPoints - 1;
+  updateLabelStats();
 });
 
 })
